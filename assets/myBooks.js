ea05397a2e905682c823f8618a080b50bf2c7ad6
Rodrigo Albuquerque Gouveia <albur883@newschool.edu>
2024-04-07 15:10:36 -0400

    Added the light box & styled books

--- assets/myBooks.js
+++ assets/myBooks.js
@@ -14,16 +14,50 @@ const renderItems = (books) => {
 		// 	conditionalClass = 'faded' // Update the variable
 		// }
 
-		let listItem =
-			`
-        <div class="book">
-          <div class="book-inner"><p>${item.title}</p></div>
-        </div>
+		let listItem = //I was working here >> 
+		`
+        <button class="book" style="width: ${item.pageNumber > 500 ? item.pageNumber / 8 : item.pageNumber / 5}px; height: calc(200px - ${item.pageNumber / 50}px); background-color: ${item.color}">
+          <p>${item.title}</p> 
+        </button>
 
-			`
+		<div class="book_description_box ">
+			<div class="book_description">
+				<div class="book_description_img"><img src="${item.coverImg}".png"></div>
+				<div class="book_description_text">
+					<h2>${item.title}</h2>
+					<h3>${item.author}</h3>
+					<p>${item.description}</p>
+				</div>
+				<button class="close">X</button>
+			</div>
+		</div>
+		`
 
 		bookList.insertAdjacentHTML('beforeend', listItem) // effectlive add the HTML? 
 	})
+
+	//function to make the book-butotn work
+	let switchButton = document.querySelectorAll('.book')
+	console.log("you got it!", switchButton)
+
+	switchButton.forEach((pressButton) => {
+		
+		pressButton.onclick = () => {
+			console.log("you are clicking!")
+			pressButton.nextElementSibling.classList.toggle('active')
+		}
+	})
+
+	//fuction to close the lightbox
+	let switchOff = document.querySelectorAll('.close')
+
+	switchOff.forEach((closeButton) => {
+
+		closeButton.onclick = () => {
+			closeButton.parentElement.parentElement.classList.remove('active')
+		}
+	})
+
 }
 
 
@@ -37,3 +71,9 @@ fetch('assets/books.json')
     //Calls the Function
 		renderItems(books)
 	})
+
+
+
+
+
+
