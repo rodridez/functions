1ef7add719d2ec174eb06e35ab0bf42111fbc07e
Rodrigo Albuquerque Gouveia <albur883@newschool.edu>
2024-04-23 20:32:01 -0400

    final big push, added the live book

--- assets/myBooks.js
+++ assets/myBooks.js
@@ -12,13 +12,14 @@ const renderItems = (books) => {
 	
 	let checkboxId = `checkbox_${item.title}`
 
-		if (item.doneReading == true) 
+		if (item.doneReading == true) // this is for if you are done with the book
 		
 		{
 			let listItem = 
 			`
 			<button class="book" style="cursor: grab; width: ${item.pageNumber > 500 ? item.pageNumber / 8 : item.pageNumber / 5}px; height: calc(200px - ${item.pageNumber / 50}px); background-color: ${item.color}; animation-delay: ${delay}s">
-			  <p>${item.title}</p> 
+				<div class="overlay hidden"></div>
+				<p>${item.title}</p> 
 			</button>
 	
 			<div class="book_description_box ">
@@ -29,7 +30,10 @@ const renderItems = (books) => {
 						<h3>${item.author}</h3>
 						<p>${item.description}</p>
 					</div>
-					<button class="close">&times;</button>
+					<div class="book_side">
+						<button class="close">&times;</button>
+						<button class="shareBook"><img src="assets/share.png"></img></button>
+					</div>
 					<div class="form_checkbox">
                             <input type="checkbox" name="${item.title}" id="${checkboxId}" checked>
                             <label class="checkbox-label" for="${checkboxId}">Done reading</label>
@@ -41,7 +45,7 @@ const renderItems = (books) => {
 			bookList.insertAdjacentHTML('beforeend', listItem) // effectlive add the HTML? 
 		}
 
-		else 
+		else //this is if you are still reading the book
 		{
 			let listItem = 
 			`
@@ -58,7 +62,10 @@ const renderItems = (books) => {
 						<h3>${item.author}</h3>
 						<p>${item.description}</p>
 					</div>
-					<button class="close">&times;</button>
+					<div class="book_side">
+						<button class="close">&times;</button>
+						<button class="shareBook"><img src="assets/share.png"></img></button>
+					</div>
 					<div class="form_checkbox">
 						<input type="checkbox" name="${item.title}" id="${item.title}">
 						<label class="checkbox-label" for="${item.title}">Done reading</label>
@@ -69,13 +76,33 @@ const renderItems = (books) => {
 			bookList.insertAdjacentHTML('beforeend', listItem) // effectlive add the HTML? 
 		}
 
+			// share function for the book
+			const shareButtons = document.querySelectorAll('.shareBook')
+
+			shareButtons.forEach((button) => {
+		
+				button.onclick = () =>  {
+					console.log("you clicked to share!")
+			
+					let url = window.location.href;
+					let encodedUrl = encodeURIComponent(url)
+
+					let message = `Hey! Look at what I am reading: ${item.title}. Check it out:`
+
+					let encodedMessage = encodeURIComponent(message);
+					let whatsappLink = `https://web.whatsapp.com/send?text=${encodedMessage} ${encodedUrl}`
+
+					window.open(whatsappLink, '_blank')
+				}
+			})
 		
 	})
 
+	//now for adding books to the calendar
 	books.forEach((item) => {
 		console.log(item.pageNumber)
 
-		if (item.doneReading == false)
+		if (item.doneReading == false) 
 		{
 			if (item.pageNumber <= 210) {
 
@@ -92,7 +119,10 @@ const renderItems = (books) => {
 						<h3>${item.author}</h3>
 						<p>${item.description}</p>
 					</div>
-					<button class="close">&times;</button>
+					<div class="book_side">
+						<button class="close">&times;</button>
+						<button class="shareBook"><img src="assets/share.png"></img></button>
+					</div>
 					<div class="form_checkbox">
 						<input type="checkbox" name="${item.title}2" id="${item.title}2">
 						<label class="checkbox-label" for="${item.title}2">Done reading</label>
@@ -120,7 +150,10 @@ const renderItems = (books) => {
 						<h3>${item.author}</h3>
 						<p>${item.description}</p>
 					</div>
-					<button class="close">&times;</button>
+					<div class="book_side">
+						<button class="close">&times;</button>
+						<button class="shareBook"><img src="assets/share.png"></img></button>
+					</div>
 					<div class="form_checkbox">
 						<input type="checkbox" name="${item.title}2" id="${item.title}2">
 						<label class="checkbox-label" for="${item.title}2">Done reading</label>
@@ -184,7 +217,7 @@ const renderItems = (books) => {
 	switchOff.forEach((closeButton) => {
 
 		closeButton.onclick = () => {
-			closeButton.parentElement.parentElement.classList.remove('active')
+			closeButton.parentElement.parentElement.parentElement.classList.remove('active')
 		}
 	})
 
@@ -229,6 +262,187 @@ const renderItems = (books) => {
 		console.log ("you closed the call!")
 			calBox.classList.remove('active')
 		}
+
+	// share function for the whole page
+	const shareButton = document.querySelector('#sharePage')
+
+	shareButton.onclick = () => {
+		console.log("you clicked to shar page!")
+	
+			let url = window.location.href;
+			let encodedUrl = encodeURIComponent(url)
+
+			let message = `Hey! Look at my book collection:`
+
+			let encodedMessage = encodeURIComponent(message);
+			let whatsappLink = `https://web.whatsapp.com/send?text=${encodedMessage} ${encodedUrl}`
+
+			window.open(whatsappLink, '_blank')
+	}
+
+	
+}
+
+
+
+//trying to play with the live form thingy
+const submitBook = document.querySelector('form')
+const bookList = document.getElementById('books-place')
+		
+submitBook.onsubmit = (event) => {
+	event.preventDefault()
+
+	let bookTitle = document.getElementById('book-title')
+	let bookAuthor = document.getElementById('author')
+	let bookPageNumber = document.getElementById('page-number')
+	let bookCoverImg = document.getElementById('cover-image')
+	let bookDescription = document.getElementById('description')
+	let bookSpineColor = document.getElementById('spine-color').value
+	let bookDoneReading = document.getElementById('done_reading').checked
+
+	let buttonClass = bookDoneReading ? 'book' : 'book-reading'
+	//let backgroundColor = bookDoneReading ? bookSpineColor : '#f1f7f7'
+
+	let listItem = 
+		`
+		<button class="${buttonClass}" style="cursor: grab; width: ${bookPageNumber.value > 500 ? bookPageNumber.value / 8 : bookPageNumber.value / 5}px; height: calc(200px - ${bookPageNumber.value / 50}px); background-color: ${bookSpineColor};">
+			<div class="overlay"></div>
+			<p>${bookTitle.value}</p> 
+		</button>
+
+		<div class="book_description_box ">
+			<div class="book_description">
+				<div class="book_description_img"><img src="${bookCoverImg.value}".png"></div>
+				<div class="book_description_text">
+					<h2>${bookTitle.value}</h2>
+					<h3>${bookAuthor.value}</h3>
+					<p>${bookDescription.value}</p>
+				</div>
+				<div class="book_side">
+					<button class="close">&times;</button>
+					<button class="shareBook"><img src="assets/share.png"></img></button>
+				</div>
+				<div class="form_checkbox">
+						<input type="checkbox" name="newbook" id="newbook" ${bookDoneReading ? 'checked' : ''}>
+						<label class="checkbox-label" for="newbook">Done reading</label>
+				</div>
+			</div>
+		</div>
+		`
+		bookList.insertAdjacentHTML('afterbegin', listItem)
+
+		if (!bookDoneReading) {
+			
+			let readingBooks = document.getElementById('readingBooks')
+			let readingBookItem =
+				
+				`
+				<li id="book-calendar" class="new_book" style="height: calc(2 * ${bookPageNumber.value}px / 20); background-color: ${bookSpineColor};"><p>${bookTitle.value}</p></li>
+	
+				<div class="book_description_box">
+					<div class="book_description">
+						<div class="book_description_img"><img src="${bookCoverImg.value}".png"></div>
+						<div class="book_description_text">
+							<h2>${bookTitle.value}</h2>
+							<h3>${bookAuthor.value}</h3>
+							<p>${bookDescription.value}</p>
+						</div>
+						<div class="book_side">
+							<button class="close">&times;</button>
+							<button class="shareBook"><img src="assets/share.png"></img></button>
+						</div>
+						<div class="form_checkbox" id="${bookTitle.value}">
+							<input type="checkbox" name="${bookTitle.value}2" id="${bookTitle.value}2">
+							<label class="checkbox-label" for="${bookTitle.value}2">Done reading</label>
+						</div>
+					</div>
+				</div>
+				`
+	
+				readingBooks.insertAdjacentHTML('beforeend', readingBookItem)
+
+			}
+
+		
+
+		let openForm = document.querySelector('#book_form_box')
+		openForm.classList.remove('active')
+
+
+		//function to open the lightbox for an added book???
+		let switchButton = document.querySelectorAll('.book')
+
+		switchButton.forEach((pressButton) => {
+		
+			pressButton.onclick = () => {
+				console.log("you are clicking!")
+				pressButton.nextElementSibling.classList.toggle('active')
+			}
+		})
+
+		let switchButton2 = document.querySelectorAll('.book-reading')
+
+		switchButton2.forEach((pressButton) => {
+		
+			pressButton.onclick = () => {
+				console.log("you are clicking!")
+				pressButton.nextElementSibling.classList.toggle('active')
+			}
+		})
+
+		let switchButton3 = document.querySelectorAll('#book-calendar')
+
+		switchButton3.forEach((pressButton) => {
+			
+			pressButton.onclick = () => {
+				pressButton.nextElementSibling.classList.toggle('active')
+			}
+		})
+
+		//fuction to close the lightbox <--- this is annoying
+		let switchOff = document.querySelectorAll('.close')
+
+		switchOff.forEach((closeButton) => {
+
+			closeButton.onclick = () => {
+				closeButton.parentElement.parentElement.parentElement.classList.remove('active')
+			}
+		})
+
+		
+		//now trying to change classes with the check button
+		const formCheckboxes = document.querySelectorAll('.form_checkbox input[type="checkbox"]')
+
+
+		formCheckboxes.forEach((checkbox) => {
+			checkbox.addEventListener('change', function() {
+
+				const checkboxId = this.id
+				const parentBox = this.parentElement.parentElement.parentElement.previousElementSibling
+
+				const bookButton = document.getElementById(checkboxId)
+				let overlay = document.querySelector('.overlay')
+
+				//this if is to remove the book from the calendar
+				if (parentBox.classList.contains('new_book')) {
+					parentBox.classList.add('hidden')
+				}
+
+				// this if is to change the book to book-reading and vice verse
+				if (this.checked) {
+					bookButton.parentElement.parentElement.parentElement.previousElementSibling.classList.remove('book-reading')
+					bookButton.parentElement.parentElement.parentElement.previousElementSibling.classList.add('book')
+					overlay.classList.toggle('hidden')
+				} else {
+					bookButton.parentElement.parentElement.parentElement.previousElementSibling.classList.remove('book')
+					bookButton.parentElement.parentElement.parentElement.previousElementSibling.classList.add('book-reading')
+				}
+
+			})
+		})
+	
+
+	submitBook.reset()
 }
 
 
